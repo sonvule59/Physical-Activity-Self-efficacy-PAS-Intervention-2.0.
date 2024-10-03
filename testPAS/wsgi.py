71b19073173cba1bf4b4490529b5c09bc3496db0
sonlevu59 <svu23@iastate.edu>
2024-10-03 14:08:57 -0500

    cleaning up

--- testPAS/wsgi.py
+++ testPAS/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testPAS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testpas.settings')
 
 application = get_wsgi_application()
