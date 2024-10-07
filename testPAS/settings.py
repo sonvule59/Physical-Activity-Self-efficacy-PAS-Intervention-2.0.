758fa3542334949966b81f0f3a5c38f19b20652c
sonlevu59 <svu23@iastate.edu>
2024-10-07 15:52:21 -0500

    maybe THIS IS THE ONE FIX

--- testPAS/settings.py
+++ testPAS/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+from testpas import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
@@ -55,7 +56,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'testpas',
 ]
 
 MIDDLEWARE = [
