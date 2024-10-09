e63b3be160e4bd1c1bc886266faac60f86e09c58
sonlevu59 <svu23@iastate.edu>
2024-10-09 17:01:40 -0500

    maybe THIS IS THE ONE FIX round 2

--- testPAS/settings.py
+++ testPAS/settings.py
@@ -13,10 +13,17 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 from testpas import settings
+import environ
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = Path(__file__).resolve().parent.parent
+env = environ.Env()
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, ".env")
+)
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
