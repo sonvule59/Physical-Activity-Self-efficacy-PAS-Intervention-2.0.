6a16f401c856c1510c8e3448a96b869df9b7da92
sonlevu59 <svu23@iastate.edu>
2024-10-14 15:22:04 -0500

    fixing whitenoise

--- config/settings.py
+++ config/settings.py
@@ -70,7 +70,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    
 ]
 
 # ROOT_URLCONF = 'testpas.urls'
--- config/wsgi.py
+++ config/wsgi.py
@@ -13,3 +13,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
