19213a69eff00b9a7e9cb6c02284a947c7b8ccd6
sonlevu59 <svu23@iastate.edu>
2024-10-14 16:01:35 -0500

    fix .urls errors in release log

--- config/urls.py
+++ config/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('survey/<int:survey_id>/start/', views.start_survey, name='start_survey'),
-    path('survey/<int:survey_id>/questions/', views.survey_questions, name='survey_questions'),
-    path('survey/<int:survey_id>/complete/', views.survey_complete, name='survey_complete'),
+    path('survey/<int:survey_id>/start/', testpas.views.start_survey, name='start_survey'),
+    path('survey/<int:survey_id>/questions/', testpas.views.survey_questions, name='survey_questions'),
+    path('survey/<int:survey_id>/complete/', testpas.views.survey_complete, name='survey_complete'),
     path('admin/', admin.site.urls),
     path('', include('https://git.heroku.com/testpas.git')),
 ]
