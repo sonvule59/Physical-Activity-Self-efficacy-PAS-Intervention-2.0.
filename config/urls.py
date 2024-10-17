562ad035b772a182039f0d6ff59a359ea4ff1a35
sonlevu59 <svu23@iastate.edu>
2024-10-16 20:58:16 -0500

    Fix module not found error for testpas/config. I did change structure a bit

--- config/urls.py
+++ config/urls.py
@@ -19,16 +19,14 @@ from django.contrib import admin
 # from . import views
 # from django.contrib import views as auth_views
 from django.urls import path, include
-from testpas import views as auth_views
-
-from testpas.views import *
+# from testpas import views as auth_views
 
 app_name = 'testpas'
 
 urlpatterns = [
-    path('survey/<int:survey_id>/start/', auth_views.start_survey, name='start_survey'),
-    path('survey/<int:survey_id>/questions/', auth_views.survey_questions, name='survey_questions'),
-    path('survey/<int:survey_id>/complete/', auth_views.survey_complete, name='survey_complete'),
+    # path('survey/<int:survey_id>/start/', auth_views.start_survey, name='start_survey'),
+    # path('survey/<int:survey_id>/questions/', auth_views.survey_questions, name='survey_questions'),
+    # path('survey/<int:survey_id>/complete/', auth_views.survey_complete, name='survey_complete'),
     path('admin/', admin.site.urls),
     path('', include('config.urls')),  
     path('', include('testpas.views')),  
