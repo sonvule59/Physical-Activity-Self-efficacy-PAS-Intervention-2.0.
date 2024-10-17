7541a7711fc46a8ae6ece5ccd9e48eacd8485177
sonlevu59 <svu23@iastate.edu>
2024-10-17 11:14:03 -0500

    test the worker failed to boot fix by changing wsgi file

--- config/wsgi.py
+++ config/wsgi.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.dev')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
