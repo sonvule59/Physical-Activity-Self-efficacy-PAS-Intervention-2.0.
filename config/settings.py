4e1078b06112e64c80041e6c7edbfd875b7f243a
sonlevu59 <svu23@iastate.edu>
2024-10-29 16:34:29 -0500

    launch attempt

--- config/settings.py
+++ config/settings.py
@@ -173,7 +173,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
+ALLOWED_HOSTS = []
 
+# Static files (CSS, JavaScript, Images)
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = "static/"
 
