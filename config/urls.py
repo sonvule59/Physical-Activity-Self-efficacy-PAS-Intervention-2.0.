bbaa75b885728a548e0532fd13f83dd80375fc8f
sonlevu59 <svu23@iastate.edu>
2024-11-13 17:06:25 -0600

    add stuffs

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 # from django.contrib import views as auth_views
 from django.urls import path, include
 # from testpas import views as auth_views
-from testpas import views
+from testPAS import views
 #import testpas.views as views
 # from django.contrib.auth import views as views
 app_name = 'testpas'
