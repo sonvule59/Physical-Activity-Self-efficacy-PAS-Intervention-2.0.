7c0c02defd52c945da23965fa7be60c393317ced
Son's thinkpad <svu23@iastat.edu>
2024-12-26 20:19:39 -0600

    update stuffs

--- testPAS/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-class CodeEntryForm(forms.Form):
-    code = forms.CharField(
-        max_length=50,
-        required=True,
-        widget=forms.TextInput(attrs={'class': 'form-control'})
-    )
